be9a82ec71b569d1a69bb1febeacf4625d5cbc3d
zhaojun <AB049836zhaojun@ab-insurance.com>
2018-08-22 15:48:15 +0800

    测试版本

---
+++ out/artifacts/ssm_web_Web_exploded/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello bingo World!</h2>
+</body>
+</html>
---
+++ ssm-service/src/main/java/bingo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by zhaojun on 2017/8/22.
+ */
+public class bingo {
+    public static void main(){
+
+    }
+
+}
