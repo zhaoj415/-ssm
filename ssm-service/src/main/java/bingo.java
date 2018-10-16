4419046a9a1e62c65f8f7102e16d7158bfc4da37
zhaojun <ab049836@gitlab.ab.com>
2018-10-16 09:52:11 +0800

    test

--- ssm-service/src/main/java/bingo.java
+++ ssm-service/src/main/java/bingo.java
@@ -2,8 +2,8 @@
  * Created by zhaojun on 2017/8/22.
  */
 public class bingo {
-    public static void main(){
-
+    public static void main(String[] args){
+        System.out.println(111111);
     }
 
 }
